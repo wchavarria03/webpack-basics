f84d31567e488e8d80e4e246ca2611a778ae37b9
Walter Chavarria <wchavarria03@gmail.com>
2017-02-08 22:04:10 -0600

    Add step-1 commented solutions

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
-module.exports = {
+  module.exports = {
+
   entry: './src/app.js',
+  //1
+  // entry: ['./src/app.js', './src/index.js'],
+
+  //2
+  // entry: {
+  //   app: './src/app.js',
+  //   index: './src/index.js'
+  // },
+
+  //3
+  // context: __dirname + '/src',
+  // entry: {
+  //   app: './app.js',
+  //   index: './index.js'
+  // },
   output: {
     path: './dist',
     filename: 'bundle.js'
+
+    //1
+    //filename: '[name].bundle.js'
   }
 };
