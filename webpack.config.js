55acf40816cecb2eab5835cc90be273728c6a514
Walter Chavarria <wchavarria03@gmail.com>
2017-02-12 08:26:04 -0600

    STEP-2: Update starting step code

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,10 @@
 module.exports = {
-
-  entry: './src/app.js',
-  //1
-  // entry: ['./src/app.js', './src/index.js'],
-
-  //2
-  // entry: {
-  //   app: './src/app.js',
-  //   index: './src/index.js'
-  // },
-
-  //3
-  // context: __dirname + '/src',
-  // entry: {
-  //   app: './app.js',
-  //   index: './index.js'
-  // },
+  context: __dirname + '/src',
+  entry: {
+    app: './app.js'
+  },
   output: {
-    path: './dist',
-    filename: 'bundle.js'
-
-    //1
-    //path: __dirname + '/dist',
-    //filename: '[name].bundle.js'
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js'
   }
 };
