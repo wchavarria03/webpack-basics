6176623a23d75c5708987084be7f049e784fbe61
Walter Chavarria <wchavarria03@gmail.com>
2017-02-13 22:02:21 -0600

    STEP-3: Update step fix

--- src/app.js
+++ src/app.js
@@ -10,6 +10,37 @@ function multiply(num, num2) {
   return multiplyModule(num, num2);
 }
 
-console.log('Add:', add(3, 5));
+/*LISTENERS*/
+var multiplyKey = document.getElementById('multiplyKey');
+multiplyKey.addEventListener('click', function () {
+  var num1 = getNumber1();
+  var num2 = getNumber2();
+  setResult(multiply(num1, num2));
+});
 
-console.log('Multiply:', multiply(3, 5));
+
+var sumKey = document.getElementById('sumKey');
+sumKey.addEventListener('click', function () {
+  var num1 = getNumber1();
+  var num2 = getNumber2();
+  setResult(add(num1, num2));
+});
+
+var clearKey = document.getElementById('clearKey');
+clearKey.addEventListener('click', function () {
+  setResult('');
+});
+
+/*UTILITIES*/
+function getNumber1() {
+  return parseInt(document.getElementById('number1').value, 10);
+}
+
+function getNumber2() {
+  return parseInt(document.getElementById('number2').value, 10);
+}
+
+function setResult(result) {
+  var resultScreen = document.getElementById('mainScreen');
+  resultScreen.innerText = result;
+}
